0a05a4bc636d415ea7d5ef04767efc3cf03a131e
Norah Tromp <80421914+l0z0y@users.noreply.github.com>
2024-04-27 10:45:31 +0800

    Update esptouch.js

--- www/esptouch.js
+++ www/esptouch.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  start: function (apSsid, apBssid, apPassword, deviceCountData, broadcastData, successCallback, failCallback) {
-    exec(successCallback, failCallback, "esptouch", "start", [apSsid, apBssid, apPassword, deviceCountData, broadcastData]);
+  start: function (apSsid, apPassword, deviceCountData, successCallback, failCallback) {
+    exec(successCallback, failCallback, "esptouch", "start", [apSsid,  apPassword, deviceCountData]);
   },
   stop: function (successCallback, failCallback) {
     exec(successCallback, failCallback, "esptouch", "stop", []);
